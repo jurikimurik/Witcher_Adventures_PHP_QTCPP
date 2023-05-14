8fa1e9681cb059abf5e3a86a7eaa5306fab5d031
jurikimurik <116065982+jurikimurik@users.noreply.github.com>
2023-05-14 13:22:15 +0200

    Restart of project is needed.
    Due to many complications and bad planning, this project needs to be created from the beginning.

--- QT(Tools)/EventsCreator/databasemodel.cpp
+++
@@ -1,6 +0,0 @@
-#include "databasemodel.h"
-
-DatabaseModel::DatabaseModel()
-{
-
-}
--- QT(Tools)/EventsCreator/toaction.cpp
+++
@@ -1,5 +0,0 @@
-#include "toaction.h"
-
-ToAction::ToAction(int toAction) : Action(ActionType::To),
-    m_toAction(toAction)
-{}
