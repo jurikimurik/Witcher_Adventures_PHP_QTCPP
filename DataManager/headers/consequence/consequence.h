a15da60b33bbbd76f7e106c21ca63ddbf1fad640
Jurikimurik <makovskyiyurii@gmail.com>
2023-05-20 15:54:21 +0200

    toString() and fromString() has been created and implemented.

--- DataManager/headers/consequence/consequence.h
+++ DataManager/headers/consequence/consequence.h
@@ -8,12 +8,18 @@ class Consequence : public DatabaseItem
 {
 public:
     Consequence(int id = -1, const QString &name = QString(), bool isOn = false);
+    virtual ~Consequence();
 
     bool isOn() const;
     void setOn(bool newIsOn);
 
 private:
     bool m_isOn;
+
+    // DatabaseItem interface
+public:
+    virtual QString toString();
+    static Consequence fromString(QString str);
 };
 
 #endif // CONSEQUENCE_H
--- DataManager/sources/consequence/consequence.cpp
+++ DataManager/sources/consequence/consequence.cpp
@@ -4,9 +4,29 @@ Consequence::Consequence(int id, const QString &name, bool isOn) : DatabaseItem(
     m_isOn(isOn)
 {}
 
+Consequence::~Consequence()
+{
+
+}
+
 bool Consequence::isOn() const
 {
     return m_isOn;
+}
+
+QString Consequence::toString()
+{
+    QString properties = DatabaseItem::toString();
+    QString splitter = DatabaseItem::getSplitter();
+
+    properties += QString::number(m_isOn) + splitter;
+    return properties;
+}
+
+Consequence Consequence::fromString(QString str)
+{
+    QStringList list = str.split(DatabaseItem::getSplitter());
+    return Consequence(list.takeFirst().toInt(), list.takeFirst(), list.takeFirst().toInt());
 }
 
 void Consequence::setOn(bool newIsOn)
