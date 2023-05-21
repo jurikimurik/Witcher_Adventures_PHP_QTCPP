29df38b7b538ff1f039fef9f0d7e934375908477
Jurikimurik <makovskyiyurii@gmail.com>
2023-05-21 12:43:50 +0200

    Basic implementation.

--- DataManager/headers/database/databasemodel.h
+++ DataManager/headers/database/databasemodel.h
@@ -1,11 +1,32 @@
 #ifndef DATABASEMODEL_H
 #define DATABASEMODEL_H
 
+#include <QObject>
+#include "../item/itemsmodel.h"
+#include "../character/charactermodel.h"
+#include "../consequence/consequencesmodel.h"
 
-class DatabaseModel
+class DatabaseModel : public QObject
 {
+    Q_OBJECT
+
+private:
+    ItemsModel* m_itemsModel;
+    CharacterModel* m_charactersModel;
+    ConsequencesModel* m_consequencesModel;
+
 public:
-    DatabaseModel();
+    DatabaseModel(ItemsModel *itemsModel = new ItemsModel(), CharacterModel *charactersModel = new CharacterModel(), ConsequencesModel *consequencesModel = new ConsequencesModel(), QObject *parent = nullptr);
+    QString toString();
+    static DatabaseModel* fromString(QString str);
+
+
+    ItemsModel *itemsModel() const;
+    void setItemsModel(ItemsModel *newItemsModel);
+    CharacterModel *charactersModel() const;
+    void setCharactersModel(CharacterModel *newCharactersModel);
+    ConsequencesModel *consequencesModel() const;
+    void setConsequencesModel(ConsequencesModel *newConsequencesModel);
 };
 
 #endif // DATABASEMODEL_H
--- DataManager/sources/database/databasemodel.cpp
+++ DataManager/sources/database/databasemodel.cpp
@@ -1,6 +1,47 @@
 #include "../../headers/database/databasemodel.h"
 
-DatabaseModel::DatabaseModel()
+DatabaseModel::DatabaseModel(QObject *parent)
+    : QObject{parent}
 {
 
+}
+
+ItemsModel *DatabaseModel::itemsModel() const
+{
+    return m_itemsModel;
+}
+
+void DatabaseModel::setItemsModel(ItemsModel *newItemsModel)
+{
+    m_itemsModel = newItemsModel;
+}
+
+CharacterModel *DatabaseModel::charactersModel() const
+{
+    return m_charactersModel;
+}
+
+void DatabaseModel::setCharactersModel(CharacterModel *newCharactersModel)
+{
+    m_charactersModel = newCharactersModel;
+}
+
+ConsequencesModel *DatabaseModel::consequencesModel() const
+{
+    return m_consequencesModel;
+}
+
+void DatabaseModel::setConsequencesModel(ConsequencesModel *newConsequencesModel)
+{
+    m_consequencesModel = newConsequencesModel;
+}
+
+DatabaseModel::DatabaseModel(ItemsModel *itemsModel, CharacterModel *charactersModel, ConsequencesModel *consequencesModel, QObject *parent) : QObject(parent),
+    m_itemsModel(itemsModel),
+    m_charactersModel(charactersModel),
+    m_consequencesModel(consequencesModel)
+{
+    m_itemsModel->setParent(this);
+    m_charactersModel->setParent(this);
+    m_consequencesModel->setParent(this);
 }
