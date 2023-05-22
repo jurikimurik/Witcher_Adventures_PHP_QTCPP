fc387a7c1d1a0d648a23c9b1c6f78a359c47b2d1
Jurikimurik <makovskyiyurii@gmail.com>
2023-05-22 14:04:10 +0200

    Created and implemented.

---
+++ DataManager/headers/action/action.h
@@ -0,0 +1,38 @@
+#ifndef ACTION_H
+#define ACTION_H
+
+#include <QString>
+#include "../../headers/basic/databaseitem.h"
+
+enum class ActionType {
+    NONE = 0,
+    Description = 1,
+    Agility = 2,
+    Battle = 3,
+    Reward = 4,
+    Choice = 5,
+    Dice = 6
+};
+
+class Action
+{
+private:
+    ActionType m_type;
+    QString m_data;
+    int m_idToAction;
+
+public:
+    Action(ActionType type = ActionType::NONE, const QString &data = QString(), int toAction = -1);
+    virtual Action toAction();
+    virtual QString toString();
+    static Action fromString(QString str);
+
+    ActionType type() const;
+    void setType(ActionType newType);
+    QString data() const;
+    void setData(const QString &newData);
+    int idToAction() const;
+    void setIdToAction(int newIdToAction);
+};
+
+#endif // ACTION_H
---
+++ DataManager/sources/action/action.cpp
@@ -0,0 +1,60 @@
+#include "../../headers/action/action.h"
+
+
+ActionType Action::type() const
+{
+    return m_type;
+}
+
+void Action::setType(ActionType newType)
+{
+    m_type = newType;
+}
+
+QString Action::data() const
+{
+    return m_data;
+}
+
+void Action::setData(const QString &newData)
+{
+    m_data = newData;
+}
+
+int Action::idToAction() const
+{
+    return m_idToAction;
+}
+
+void Action::setIdToAction(int newIdToAction)
+{
+    m_idToAction = newIdToAction;
+}
+
+Action::Action(ActionType type, const QString &data, int toAction) : m_type(type),
+    m_data(data),
+    m_idToAction(toAction)
+{}
+
+Action Action::toAction()
+{
+    return *this;
+}
+
+QString Action::toString()
+{
+    QString splitter = DatabaseItem::getSplitter();
+    QString properties = QString::number((int)type()) + splitter + data() + splitter + QString::number(idToAction()) + splitter;
+
+    return properties;
+}
+
+Action Action::fromString(QString str)
+{
+    QStringList props = str.split(DatabaseItem::getSplitter());
+    ActionType type = (ActionType) props.at(0).toInt();
+    QString data = props.at(1);
+    int toAction = props.at(2).toInt();
+
+    return Action(type, data, toAction);
+}
