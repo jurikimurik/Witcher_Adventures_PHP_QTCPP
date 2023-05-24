cde3370b2bb81db4ee7cbcc1e8a1b705bd126dba
Jurikimurik <makovskyiyurii@gmail.com>
2023-05-24 21:41:09 +0200

    Signals/slots/methods has been created only.

--- DataManager/forms/headers/action/actionview.h
+++ DataManager/forms/headers/action/actionview.h
@@ -2,6 +2,7 @@
 #define ACTIONVIEW_H
 
 #include <QWidget>
+#include "../../../headers/action/action.h"
 
 namespace Ui {
 class ActionView;
@@ -12,11 +13,39 @@ class ActionView : public QWidget
     Q_OBJECT
 
 public:
-    explicit ActionView(QWidget *parent = nullptr);
+    explicit ActionView(const Action &data = Action(), QWidget *parent = nullptr);
     ~ActionView();
+
+    void openAction(Action action);
+
+    static ActionView* fromAction(Action action);
+
+    Action getData() const;
+    void setData(const Action &newData);
+
+    //---------------CONNECTION BETWEEN ACTIONVIEW AND EVENTSVIEW---------------
+public slots:
+    void updateEnemies(QStringList list);
+    void updateActions(QStringList list);
+    void updateItems(QStringList list);
+
+signals:
+    void enemiesIdsNeeded();
+    void actionIdsNeeded();
+    void itemIdsNeeded();
+
+    void actionChanged(const Action &action);
+    //--------------------------------------------------------------------------
+
+private slots:
+    void on_enemyBox1_activated(int index);
+    void on_toActionIdBox1_activated(int index);
+    void on_itemBox1_activated(int index);
+    void on_d_enemyBox1_activated(int index);
 
 private:
     Ui::ActionView *ui;
+    Action data;
 };
 
 #endif // ACTIONVIEW_H
--- DataManager/forms/sources/action/actionview.cpp
+++ DataManager/forms/sources/action/actionview.cpp
@@ -1,14 +1,74 @@
 #include "../../headers/action/actionview.h"
 #include "ui_actionview.h"
 
-ActionView::ActionView(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::ActionView)
-{
-    ui->setupUi(this);
-}
-
 ActionView::~ActionView()
 {
     delete ui;
+}
+
+void ActionView::openAction(Action action)
+{
+
+}
+
+ActionView *ActionView::fromAction(Action action)
+{
+
+}
+
+void ActionView::on_enemyBox1_activated(int index)
+{
+    //WHEN "NEW ENEMY" IN BATTLE ACTION AHS BEEN CLICKED
+}
+
+
+void ActionView::on_toActionIdBox1_activated(int index)
+{
+    // WHEN "NEW ACTION" IN CHOICE ACTION HAS BEEN CLICKED
+}
+
+
+void ActionView::on_itemBox1_activated(int index)
+{
+    // WHEN "NEW ITEM" IN REWARD ACTION HAS BEEN CLICKED
+}
+
+
+void ActionView::on_d_enemyBox1_activated(int index)
+{
+    // WHEN "NEW ENEMY" IN DICE ACTION HAS BEEN CLICKED
+}
+
+Action ActionView::getData() const
+{
+    return data;
+}
+
+void ActionView::setData(const Action &newData)
+{
+    data = newData;
+    openAction(data);
+}
+
+void ActionView::updateActions(QStringList list)
+{
+
+}
+
+void ActionView::updateItems(QStringList list)
+{
+
+}
+
+void ActionView::updateEnemies(QStringList list)
+{
+
+}
+
+
+ActionView::ActionView(const Action &data, QWidget *parent) : QWidget(parent),
+    ui(new Ui::ActionView),
+    data(data)
+{
+    ui->setupUi(this);
 }
