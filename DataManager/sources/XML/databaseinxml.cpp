fd9ba1cf4eea7712e66ab71033d0dd5a28d6d898
Jurikimurik <makovskyiyurii@gmail.com>
2023-05-31 12:18:05 +0200

    XML save for items has been added.

--- DataManager/sources/XML/databaseinxml.cpp
+++ DataManager/sources/XML/databaseinxml.cpp
@@ -14,7 +14,52 @@ void DatabaseInXML::setModel(DatabaseModel *newModel)
 
 void DatabaseInXML::saveToFile(const QString &file)
 {
+    QFile xmlFile(file);
+    if(!xmlFile.open(QIODevice::WriteOnly))
+        throw std::runtime_error("Can't open file " + file.toStdString() + " for writing!");
 
+    QXmlStreamWriter writer(&xmlFile);
+    writer.setAutoFormatting(true);
+    writer.writeStartDocument();
+
+    writer.writeStartElement(ItemsModel::getModelSplitter()); // ItemsModel
+
+    //Items
+    ItemsModel* itemModel = m_model->itemsModel();
+    QList<Item> items = itemModel->values();
+    for(const auto& elem : items)
+    {
+        writer.writeStartElement(QString::number(elem.id())); //Item
+        writer.writeAttribute("Name", elem.name());
+
+        writer.writeStartElement(elem.type().typeName);         //ItemType
+        writer.writeAttribute("Description", elem.type().typeDescription);
+        writer.writeEndElement();                               //ItemType
+
+        writer.writeAttribute("Description", elem.description());
+        writer.writeAttribute("Image", elem.imageName());
+        writer.writeAttribute("Money", QString::number(elem.money()));
+
+        //BUFFS HERE
+        for(const auto& buff : elem.buffs())
+        {
+            writer.writeStartElement(buff.name());  // BUFF
+            writer.writeAttribute("Duration", QString::number(buff.duration()));
+
+            writer.writeStartElement("Attributes"); // ATTRIBUTES
+            for(int i = 0; i < AttributesNames.size(); ++i)
+                writer.writeAttribute(AttributesNames.at(i), QString::number(buff.changedAttributes().*AttributesPointers.at(i)));
+            writer.writeEndElement();   // ATTRIBUTES
+
+            writer.writeEndElement();   // BUFF
+        }
+
+        writer.writeEndElement();   //Item
+    }
+    writer.writeEndElement(); // ItemsModel
+
+
+    writer.writeEndDocument(); //All document
 }
 
 void DatabaseInXML::readFromFile(const QString &file)
