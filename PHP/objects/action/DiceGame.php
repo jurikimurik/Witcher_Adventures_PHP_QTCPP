761d88e7a12f83e0b266551b9d974ae137c3c6e9
Jurikimurik <makovskyiyurii@gmail.com>
2023-06-13 19:38:10 +0200

    rollTheDice(), addDice(), removeDice() has been added.

--- PHP/objects/action/DiceGame.php
+++ PHP/objects/action/DiceGame.php
@@ -28,6 +28,62 @@ class DiceGame
 
 
     //------------------GAME MECHANICS------------------
+    public function addDice(string $playerName = "")
+    {
+        if(isset($playerName))
+        {
+            // GIVE DICE ONLY FOR HIM
+            $this->allPlayerDices[$playerName][] = 3;
+        } else {
+            // GIVE DICE TO EVERYBODY
+            foreach ($this->allPlayerDices as $name => $array)
+            {
+                $this->allPlayerDices[$name][] = 3;
+            }
+        }
+    }
+    public function removeDice(string $playerName = "")
+    {
+        if(isset($playerName))
+        {
+            // REMOVE DICE ONLY FOR HIM
+            array_splice($this->allPlayerDices[$playerName],count($this->allPlayerDices[$playerName])-1,1);
+        } else {
+            // REMOVE DICE TO EVERYBODY
+            foreach ($this->allPlayerDices as $name => $array)
+            {
+                array_splice($this->allPlayerDices[$name],count($this->allPlayerDices[$name])-1,1);
+            }
+        }
+    }
+    public function rollTheDice(string $playerName = "")
+    {
+        if(isset($playerName))
+        {
+            // ROLL THE DICE FOR SPECIFIED PLAYER
+            $allDices = array();
+            $playerDices = $this->allPlayerDices[$playerName];
+            for ($i = 0; $i < count($playerDices); $i++)
+            {
+                $allDices[$i] = rand(1, 6);
+            }
+            $this->allPlayerDices[$playerName] = $allDices;
+        } else {
+            // ROLL FOR EVERYBODY
+            foreach ($this->allPlayerDices as $name => $diceArray)
+            {
+                $allDices = array();
+                $playerDices = $this->allPlayerDices[$name];
+                for ($i = 0; $i < count($playerDices); $i++)
+                {
+                    $allDices[$i] = rand(1, 6);
+                }
+                $this->allPlayerDices[$name] = $allDices;
+            }
+        }
+    }
+
+
 
     //------------------VISUALS-------------------------
     public function getVisualTextBlock() : string
